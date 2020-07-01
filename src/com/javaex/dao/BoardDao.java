3f748555d4a80b5040ed44913abe7c506aa219c0
realseok2 <kts4315@naver.com>
2020-07-01 21:58:41 +0900

    hh24변경완료, 페이징 아직 못함

--- src/com/javaex/dao/BoardDao.java
+++ src/com/javaex/dao/BoardDao.java
@@ -198,7 +198,7 @@ public class BoardDao {
 			query += " 				b.title,";
 			query += " 				b.content,";
 			query += " 				b.hit,";
-			query += " 				to_char(b.reg_date, 'yy-mm-dd hh:mm') reg_date,";
+			query += " 				to_char(b.reg_date, 'yy-mm-dd hh24:mm') reg_date,";
 			query += " 				b.user_no,";
 			query += " 				u.name";
 			query += " FROM        	board b, users u";
@@ -245,7 +245,7 @@ public class BoardDao {
 			query += " 				b.title,";
 			query += " 				b.content,";
 			query += " 				b.hit,";
-			query += " 				to_char(b.reg_date, 'yy-mm-dd hh:mm') reg_date,";
+			query += " 				to_char(b.reg_date, 'yy-mm-dd hh24:mm') reg_date,";
 			query += " 				b.user_no,";
 			query += " 				u.name";
 			query += " FROM        	board b, users u";
@@ -326,7 +326,7 @@ public class BoardDao {
 			query += " 					b.title,";
 			query += " 					b.content,";
 			query += " 					b.hit,";
-			query += " 					to_char(b.reg_date, 'yy-mm-dd hh:mm') reg_date,";
+			query += " 					to_char(b.reg_date, 'yy-mm-dd hh24:mm') reg_date,";
 			query += " 					b.user_no,";
 			query += " 					u.name";
 			query += " FROM        		board b, users u";
